6361a0fc3cdc8f6cf0e64319f6ff10367852b59f
GoodChair <ralis3333@gmail.com>
2025-01-11 18:00:53 +0200

    component file

---
+++ electric.js
@@ -0,0 +1,44 @@
+const e = [
+    {
+        "device": "sensor",
+        "type": "flow",
+        "name": "FS3000",
+        "supply voltage": 3.3
+    },
+    {
+        "device": "sensor",
+        "type": "holla efect",
+        "name": "Flange Mount Hall Effect",
+        "supply voltage": 24
+    },
+    {
+        "device": "sensor",
+        "type": "holla efect",
+        "name": "Panel Hall Effect Sensor",
+        "supply voltage": 24
+    },
+    {
+        "device": "sensor",
+        "type": "montion",
+        "name": "PEC16",
+        "supply voltage": 5
+    },
+    {
+        "device": "sensor",
+        "type": "montion",
+        "name": "ECW1J",
+        "supply voltage": 10
+    },
+    {
+        "device": "sensor",
+        "type": "proximity",
+        "name": "ultrasonic smooth barrel",
+        "supply voltage": 20
+    },
+    {
+        "device": "sensor",
+        "type": "tenzorezistor",
+        "name": "FSR03BE",
+        "supply voltage": 3.3
+    }
+]
