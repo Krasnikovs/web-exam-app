35bf167dfe3f513ab69a0ef7d805bd1062ccbbfe
GoodChair <ralis3333@gmail.com>
2025-01-11 17:02:35 +0200

    added tables

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
     const sideBarButton = document.getElementById('sidenav-activate');
+    const readingButton = document.getElementById('create-card');
+    const newProjectButton = document.getElementById('new-project');
+    const existingProjectButton = document.getElementById('exist-project');
+    const projectNameInput = document.getElementById('project-name-input');
+    const projectNameError = document.getElementById('project-name-error');
 
     var sideBarPositionClosed = true;
 
@@ -14,6 +19,75 @@ window.addEventListener('DOMContentLoaded', (event) => {
             sideBarPositionClosed = true;
         }
     });
+
+    var readingFormClosed = true;
+
+    readingButton.addEventListener('click', () => {
+        if (readingFormClosed) {
+            document.getElementById('reading-form').style.display = 'block';
+            readingFormClosed = false;
+        } else {
+            document.getElementById('reading-form').style.display = 'none';
+            readingFormClosed = true;
+        }
+        
+    });
+
+    var projectNameInputClosed = true;
+    var projectType = null;
+
+    newProjectButton.addEventListener('click', () => {
+        if (projectType === null) {
+            projectType = 'new';
+            document.getElementById('project-name').style.display = 'block';
+            projectNameInputClosed = false;
+        } else if (projectType === 'new' && projectNameInputClosed === false) {
+            document.getElementById('project-name').style.display = 'none';
+            projectType = null;
+            projectNameInputClosed = true;
+        } else if (projectType === 'exist') {
+            projectType = 'new';
+        }
+    });
+
+    existingProjectButton.addEventListener('click', () => {
+        if (projectType === null) {
+            projectType = 'exist';
+            document.getElementById('project-name').style.display = 'block';
+            projectNameInputClosed = false;
+        } else if (projectType === 'exist' && projectNameInputClosed === false) {
+            document.getElementById('project-name').style.display = 'none';
+            projectType = null;
+            projectNameInputClosed = true;
+        } else if (projectType === 'new') {
+            projectType = 'exist';
+        }
+    });
+
+    projectName = []
+
+    projectNameInput.addEventListener('input', () => {
+        projectNameError.textContent = '';
+        if (projectNameInput.value.length < 3) {
+            projectNameError.textContent = 'Error. Name entered too short';
+        }
+
+        if (projectType === 'new') {
+            for (let i = 0; i < projectName.length; i++) {
+                if (projectName == projectNameInput.value) {
+                    projectNameError.textContent = 'Error. Project already exists';
+                }
+            }
+        } 
+        if (projectType === 'exist') {
+            for (let i = 0; i < projectName.length; i++) {
+                if (projectName != projectNameInput.value) {
+                    projectNameError.textContent = 'Error. Project doesnt exist';
+                }
+            }
+        }
+    });
+
 
     function openNav() {
         document.getElementById("sideBar").style.width = "300px";
