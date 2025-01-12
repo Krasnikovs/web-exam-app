3c71d6f87c4c3c783022c61e4795cd508002d70c
GoodChair <ralis3333@gmail.com>
2025-01-12 14:29:10 +0200

    final day

--- electric.js
+++ electric.js
@@ -3,42 +3,70 @@ const e = [
         "device": "sensor",
         "type": "flow",
         "name": "FS3000",
-        "supply voltage": 3.3
+        "supply voltage": 3.3,
+        "img": "https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_1.0,f_auto,q_auto,w_700/c_pad,w_700/Y2494368-01"
     },
     {
         "device": "sensor",
         "type": "holla efect",
         "name": "Flange Mount Hall Effect",
-        "supply voltage": 24
+        "supply voltage": 24,
+        "img": "https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_1.0,f_auto,q_auto,w_700/c_pad,w_700/Y2773624-01"
     },
     {
         "device": "sensor",
         "type": "holla efect",
         "name": "Panel Hall Effect Sensor",
-        "supply voltage": 24
+        "supply voltage": 24,
+        "img": "https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_1.0,f_auto,q_auto,w_700/c_pad,w_700/Y2713086-01"
     },
     {
         "device": "sensor",
         "type": "montion",
         "name": "PEC16",
-        "supply voltage": 5
+        "supply voltage": 5,
+        "img": "https://i.ebayimg.com/images/g/yYoAAOSwChJjjaE9/s-l1200.jpg"
     },
     {
         "device": "sensor",
         "type": "montion",
         "name": "ECW1J",
-        "supply voltage": 10
+        "supply voltage": 10,
+        "img": "https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_1.0,f_auto,q_auto,w_700/c_pad,w_700/R6928617-01"
     },
     {
         "device": "sensor",
         "type": "proximity",
         "name": "ultrasonic smooth barrel",
-        "supply voltage": 20
+        "supply voltage": 20,
+        "img": "https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_1.0,f_auto,q_auto,w_700/c_pad,w_700/F2370799-01"
     },
     {
         "device": "sensor",
         "type": "tenzorezistor",
         "name": "FSR03BE",
-        "supply voltage": 3.3
+        "supply voltage": 3.3,
+        "img": "https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_1.0,f_auto,q_auto,w_700/c_pad,w_700/W2047509-01"
+    },
+    {
+        "device": "sensor",
+        "type": "tempereture-humidity",
+        "name": "AM2302",
+        "supply voltage": 3.3,
+        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8rGH2IYof6_T4pFJDdADbM01wg6Q-mEV50A&s"
+    },
+    {
+        "device": "sensor",
+        "type": "sound",
+        "name": "LM393",
+        "supply voltage": 5,
+        "img": "https://components101.com/sites/default/files/components/Sound-Detection-Sensor-Module.jpg"
+    },
+    {
+        "device": "sensor",
+        "type": "gyroscope-accelerometer",
+        "name": "MPU6050",
+        "supply voltage": 3.3,
+        "img": "https://www.electronicwings.com/storage/PlatformSection/TopicContent/138/description/MPU6050%20Module.jpg"
     }
 ]
--- script.js
+++ script.js
@@ -6,10 +6,107 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const existingProjectButton = document.getElementById('exist-project');
     const projectNameInput = document.getElementById('project-name-input');
     const projectNameError = document.getElementById('project-name-error');
+    const filterButton = document.getElementById('filter');
+    const componentList = document.getElementById('component-list');
+    const addButton = document.getElementById('add');
+    const componentListError = document.getElementById('component-list-error');
+    const emptyBody = document.getElementById('empty-body');
+    const cardHolder = document.getElementById('card-holder');
+
 
     var sideBarPositionClosed = true;
 
-    sideBarButton.addEventListener('click', () =>{
+    let nameLookup = {}
+
+    e.forEach(component => {
+        const listComponent = document.createElement('li');
+        
+        componentList.appendChild(listComponent);
+        
+        const componentDiv = document.createElement('div');
+        const componentListLabel = document.createElement('label');
+        const componentListRadio = document.createElement('input');
+        // const componentImg = document.createElement('img');
+        const componentName = document.createElement('p');
+        componentDiv.classList.add('component-list-item');
+
+        componentListRadio.type = 'radio';
+        componentListRadio.name = 'component-to-add';
+        componentListRadio.id = `component-list-radio-${component.name}`;
+        componentListLabel.htmlFor = `component-list-radio-${component.name}`;
+        componentDiv.appendChild(componentListRadio);
+        componentDiv.appendChild(componentListLabel);
+        listComponent.appendChild(componentDiv);
+
+        // componentImg.src = component.img;
+        // componentListLabel.appendChild(componentImg);
+
+        componentName.innerHTML = component.name;
+        componentListLabel.appendChild(componentName);
+
+
+
+        if (!(component.name in nameLookup)) {
+            nameLookup[component.name] = 1;
+        }
+    })
+
+    const componentNames = Object.keys(nameLookup).sort();
+
+    let radioChecked = false;
+
+    addButton.addEventListener('click', () => {
+        componentListError.textContent = '';
+        cardAdd();
+    });
+
+    function cardAdd() {
+        for (let i = 0; i < componentNames.length; i++) {
+            // console.log(componentNames[i])
+            if (document.getElementById(`component-list-radio-${componentNames[i]}`).checked) {
+                cardValue = componentNames[i];
+                radioChecked = true;
+                break;
+            }
+        }
+        if (radioChecked) {
+            console.log(cardValue);
+            createCard(cardValue);
+        } else {
+            componentListError.textContent = 'Error. No component chosen';
+        }
+    }
+
+    function createCard(cardValue) {
+        emptyBody.style.display = 'none';
+
+        const sensorCard = document.createElement('div');
+        sensorCard.classList.add('sensor-card');
+        cardHolder.appendChild(sensorCard);
+        
+        const sensorCardDataContainer = document.createElement('div');
+        sensorCardDataContainer.classList.add('sensor-card-data-container');
+
+        const sensorCardNameContainer = document.createElement('div');
+        sensorCardNameContainer.classList.add('sensor-card-name-container');
+
+        sensorCard.appendChild(sensorCardDataContainer);
+        sensorCard.appendChild(sensorCardNameContainer);
+
+        const sensorCardData = document.createElement('p');
+        for (let i = 0; i < e.length; i++) {
+            if (e[i].name === cardValue) {
+                sensor = e[i];
+            }
+        }
+        
+        const sensorCardName = document.createElement('h4');
+        sensorCardName.innerHTML = `${cardValue}`;
+        sensorCardNameContainer.appendChild(sensorCardName);
+    }
+    
+
+    sideBarButton.addEventListener('click', () => {
 
         if (sideBarPositionClosed) {
             openNav();
